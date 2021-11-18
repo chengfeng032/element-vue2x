cf37c06e69a373d1410ea319040d438fade51a95
Issac <645778875@qq.com>
2021-11-19 00:29:40 +0800

    新增checkbox组件
    新增checkbox组件

---
+++ packages/checkbox/src/checkbox.vue
@@ -0,0 +1,24 @@
+<template>
+	<label
+		class="el-checkbox"
+		:class="[
+			border && checkboxSize ? `el-chekbox-${checkboxSize}` : ''
+			{
+				'is-disabled': disabled,
+				
+			}
+		]">
+		<span class="el-checkbox__input">
+			<span class="el-checkbox__inner"></span>
+			<input v-if="trueLabel || falseLabel"/>
+			<input v-else />
+		</span>
+		<span class="el-checkbox__label" v-if="$slots.default || label">
+			<slot></slot>
+			<template v-if="!$slots.default">{{ label }}</template>
+		</span>
+	</label>
+</template>
+
+<script>
+</script>
